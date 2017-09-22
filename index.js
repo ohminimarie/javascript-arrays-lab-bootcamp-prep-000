b4d0c02d6faca54b35b7892d1ab0cddb7034d6af
Marie Alcantara <mcjalcantara@gmail.com>
2017-09-22 23:09:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten()  {
+  
+}
